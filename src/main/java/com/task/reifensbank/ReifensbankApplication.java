7f715dae147ee0992053538ab2c1971f31ed9cda
Lukas Rybnikar <lukas.rybnikar@gmail.com>
2025-10-27 20:29:46 +0100

    Added maven springboot project to repository

---
+++ src/main/java/com/task/reifensbank/ReifensbankApplication.java
@@ -0,0 +1,13 @@
+package com.task.reifensbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReifensbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReifensbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/reifensbank/ReifensbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.reifensbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReifensbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
