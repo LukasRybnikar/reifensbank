21e434e5752610d2af4d4af95a2d130e3c669ddf
Lukas Rybnikar <lukas.rybnikar@gmail.com>
2025-10-28 14:32:45 +0100

    Added openapi and refactor of pom and all needed dependancies. Made app build .war for deploy

--- src/main/java/com/task/reifensbank/ReifensbankApplication.java
+++ src/main/java/com/task/reifensbank/ReifensbankApplication.java
@@ -2,12 +2,17 @@ package com.task.reifensbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ReifensbankApplication {
+public class ReifensbankApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+        return app.sources(ReifensbankApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReifensbankApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ReifensbankApplication.class, args);
+    }
 }
